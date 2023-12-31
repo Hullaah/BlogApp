b860a014fb9590208a4fa16669a6a905473fbf3e
Hullaah <umaradelo1.247@gmail.com>
2023-12-31 22:07:41 +0100

    Created the login and signup page

--- blog.py
+++ blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "8970762fb1ac44061fd47e0182701a15"
 
 posts = [
     {
@@ -19,12 +21,29 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("You have Logged in Successfully.", "success")
+        return redirect(url_for("home"))
+    flash("Login Unsuccessful. Please enter valid credentials.", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
